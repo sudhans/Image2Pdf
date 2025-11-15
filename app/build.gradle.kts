2b27690bf8c67b7fb2f7368ba261b6f0cf8cb3c6
msd <sudhans@gmail.com>
2025-11-15 21:30:59 +0530

    - Update App Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.msd.image2pdf"
         minSdk = 33
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.1"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
